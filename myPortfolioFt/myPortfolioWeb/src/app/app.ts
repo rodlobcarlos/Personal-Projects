fe3ab6c053ff7e23b70e726696910683a6867d86
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-04-02 00:54:59 +0200

    new project from the beginning

--- myPortfolioFt/myPortfolioWeb/src/app/app.ts
+++ myPortfolioFt/myPortfolioWeb/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('myPortfolio');
+  protected readonly title = signal('myPortfolioWeb');
 }
