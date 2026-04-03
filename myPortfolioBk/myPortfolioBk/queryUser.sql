8bdf836a2da61efd6a0d2109f3b362bf129ce2f3
rodlobcarlos <rodlobcarlos@gmail.com>
2026-04-03 20:50:32 +0200

    Update queryUser.sql

--- myPortfolioBk/myPortfolioBk/queryUser.sql
+++ myPortfolioBk/myPortfolioBk/queryUser.sql
@@ -1,3 +1,3 @@
-ALTER USER 'carlos'@'localhost' IDENTIFIED WITH mysql_native_password BY 'crldb05@rl21';
+ALTER USER 'carlos'@'localhost' IDENTIFIED WITH mysql_native_password BY 'crldb05@rl21project';
 GRANT ALL PRIVILEGES ON portfoliodb.* TO 'carlos'@'localhost';
 FLUSH PRIVILEGES;
