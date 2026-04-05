aecc56d85457733d4f3a2c2736cd912b9be55e5d
rodlobcarlos <rodlobcarlos@gmail.com>
2026-04-05 12:31:02 +0200

    changes

--- Portfolio/myPortfolioBk/myPortfolioBk/queryUser.sql
+++ Portfolio/myPortfolioBk/myPortfolioBk/queryUser.sql
@@ -4,11 +4,16 @@ FLUSH PRIVILEGES;
 
 create database portfoliodb;
 use portfoliodb;
+drop database portfoliodb;
 
 INSERT INTO project (title, description, tech_stack, github_url) 
 VALUES (
   'Personal Portfolio', 
-  'A full-stack portfolio built with Angular 19 and Spring Boot 4.', 
+  'Full-stack portfolio built with Angular 19 and Spring Boot 4, here you can see my profile in this sector and the code of the website you are navegating now with all the information I used for build this site.', 
   'Angular, Java, MySQL', 
-  'https://github.com/rodlobcarlos/myPortfolio'
+  'github.com/rodlobcarlos/Personal-Projects/tree/main/Portfolio'
 );
+
+UPDATE projects 
+SET github_url = 'github.com/rodlobcarlos/Personal-Projects/tree/main/Portfolio' 
+WHERE title = 'Personal Portfolio';
