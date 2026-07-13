ddefb806ab22dba6df085e4100ce32247a644cc1
rodlobcarlos <rodlobcarlos@gmail.com>
2026-07-13 19:15:55 +0200

    Update app.routes.ts

--- Portfolio/myPortfolioFt/myPortfolioWeb/src/app/app.routes.ts
+++ Portfolio/myPortfolioFt/myPortfolioWeb/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { MyPortfolioComponent } from './components/my-portfolio/my-portfolio';
 
 export const routes: Routes = [
   // When the path is empty, show the portfolio component
-  { path: 'crl-dev', component: MyPortfolioComponent }, 
-  { path: '**', redirectTo: 'crl-dev' } // Redirect to the portfolio component for any unknown paths
+  { path: 'CRLdev', component: MyPortfolioComponent }, 
+  { path: '**', redirectTo: 'CRLdev' } // Redirect to the portfolio component for any unknown paths
 ];
