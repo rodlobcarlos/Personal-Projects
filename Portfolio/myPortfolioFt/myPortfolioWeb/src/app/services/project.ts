4b5dcbf553801da9331de61a2c376549543a4229
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-08-04 23:18:29 +0200

    Update API URL in ProjectService

--- Portfolio/myPortfolioFt/myPortfolioWeb/src/app/services/project.ts
+++ Portfolio/myPortfolioFt/myPortfolioWeb/src/app/services/project.ts
@@ -13,7 +13,7 @@ export interface Project {
 export class ProjectService {
   private http = inject(HttpClient);
   // Ensure this matches your Spring Boot @RequestMapping
-  private url = 'https://personal-projects-1bjr.onrender.com'; 
+  private url = 'https://personal-projects-1bjr.onrender.com/api/projects'; 
 
   // The Signal that your components will listen to
   projects = signal<Project[]>([]);
