5f97d280c667df077f93732df1a10d556b84d1d5
rodlobcarlos <rodlobcarlos@gmail.com>
2026-08-04 22:01:03 +0200

    fix: CrossOrigin

--- Portfolio/myPortfolioBk/myPortfolioBk/src/main/java/com/project/myPortfolioBk/controller/ProjectController.java
+++ Portfolio/myPortfolioBk/myPortfolioBk/src/main/java/com/project/myPortfolioBk/controller/ProjectController.java
@@ -13,7 +13,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("api/projects")
-@CrossOrigin(origins = "https://crldev.up.railway.app") // for front connected
+@CrossOrigin(origins = "https://personal-projects-l4lddiurq-rodlobcarlos-5951s-projects.vercel.app") // for front connected
 public class ProjectController {
 
     private ProjectService projectService;
