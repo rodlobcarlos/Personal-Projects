2cbe955e09b9a55f92f0ab123b0f28dd25609662
rodlobcarlos <rodlobcarlos@gmail.com>
2026-09-03 12:43:52 +0200

    Add bash script to retrieve system information including CPU, RAM, and disk space

---
+++ Mini_proyecto_bash/script.bash
@@ -0,0 +1,6 @@
+# Script en bash para obtener la información de estos valores: CPU, RAM, Disco, Servicios, Connectividad y Espacio disponibles.
+
+echo "Información del sistema:"
+powershell.exe -Command "Get-CimInstance Win32_Processor | Format-List *"
+powershell.exe -Command "Get-CimInstance Win32_ComputerSystem | Format-List *"
+powershell.exe -Command "Get-CimInstance Win32_LogicalDisk | Where-Object {$_.DeviceID -eq 'C:'} | Format-List *"
