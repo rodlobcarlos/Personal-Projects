a46b024d9380fa6c918aa2989bcffb1e0d53796b
rodlobcarlos <rodlobcarlos@gmail.com>
2026-09-04 16:11:41 +0200

    Fix formatting in system information report and script output

--- Mini_proyecto_bash/script.sh
+++ Mini_proyecto_bash/script.sh
@@ -2,30 +2,30 @@
 
 # Script en bash para obtener la información de estos valores: CPU, RAM, Disco, Servicios, Connectividad y Espacio disponibles.
 {
-    echo "=== INFORMACIÓN DE LA CPU ==="
+    echo "=== INFORMACION DE LA CPU ==="
     # Muestra el modelo de la CPU en el sistema
     grep -m 1 "model name" /proc/cpuinfo | cut -d: -f2 | xargs
 
     echo # Agrega una línea en blanco para separar la información
-    echo "=== INFORMACIÓN DE LA RAM ==="
+    echo "=== INFORMACION DE LA RAM ==="
     # Muestra la información de la memoria RAM disponible en el sistema
     cat /proc/meminfo | head -n 4
 
     echo
-    echo "=== INFORMACIÓN DEL DISCO ==="
+    echo "=== INFORMACION DEL DISCO ==="
     df -h
 
     echo
-    echo "=== INFORMACIÓN DE LOS SERVICIOS ==="
+    echo "=== INFORMACION DE LOS SERVICIOS ==="
     # Muestra los primeros 10 servicios en ejecución en Windows usando PowerShell
     powershell.exe -Command "Get-Service | Where-Object {\$_.Status -eq 'Running'} | Select-Object -First 10 | Format-Table Name, DisplayName, Status" | tr -d '\r'
 
-    echo "=== INFORMACIÓN DE CONECTIVIDAD ==="
+    echo "=== INFORMACION DE CONECTIVIDAD ==="
     # Muestra las conexiones TCP activas y los puertos que están escuchando (listening)
     netstat -ano | grep -E "LISTENING|ESTABLISHED" | head -n 15
 
     echo
-    echo "=== INFORMACIÓN DE ESPACIO DISPONIBLE ==="
+    echo "=== INFORMACION DE ESPACIO DISPONIBLE ==="
     # Muestra la información de los discos físicos en el sistema usando PowerShell
     powershell.exe -Command "Get-PhysicalDisk | Select-Object DeviceId, Model, MediaType, @{Name='Size_GB';Expression={[Math]::Round(\$_.Size / 1GB, 2)}}" | tr -d '\r';
 } > informe.txt
